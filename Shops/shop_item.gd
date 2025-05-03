d761ece7b2b21e8751573db44033f29b59ab53d9
kikis05 <kirthis2@illinois.edu>
2025-05-03 01:19:02 -0500

    shop changes, dialogues

---
+++ Shops/shop_item.gd
@@ -0,0 +1,66 @@
+extends TextureButton
+
+class_name ShopItem
+
+@export var item_name: String = "item"
+@export var description: String = "a helpful item"
+@export var cost: int = 0
+@export var sprite_path: String
+
+@export var powerup_type: String = "health_increase" #change depending on what kind of powerup it is
+
+#@export var health_increase: int #should be multiple of 2
+#@export var speed_increase: int
+#@export var sword_attack_increase: int
+#@export var bullet_attack_increase: int
+#@export var bullet_distance_increase: int
+
+@export var powerup_value: int
+
+@onready var itemImage = $TextureRect/Item
+
+func set_data(image_, name_, cost_, description_, powerup_type_, powerup_amount_):
+	sprite_path = image_
+	print(sprite_path)
+	var img = load(sprite_path) # Replace with the actual path to your PNG file
+	itemImage.texture = img
+	print ("texture should be set??")
+	item_name = name_
+	cost = cost_
+	description = description_
+	powerup_type = powerup_type_
+	powerup_value = powerup_amount_
+	
+func _ready():
+	if (sprite_path) :
+		print(sprite_path)
+		var img = load(sprite_path) # Replace with the actual path to your PNG file
+		itemImage.texture = img
+		print ("texture should be set??")
+
+#can add other powerups here
+func get_powerup_type():
+	return powerup_type
+
+func get_powerup_value():
+	#match powerup_type:
+		#"health_increase":
+			#return health_increase
+		#"speed_increase":
+			#return speed_increase
+		#"sword_attack_increase":
+			#return sword_attack_increase
+		#"bullet_attack_increase":
+			#return bullet_attack_increase
+		#"bullet_distance_increase":
+			#return bullet_distance_increase
+	return powerup_value
+	
+func get_cost():
+	return cost
+	
+func get_description():
+	return description
+
+func get_item_name():
+	return item_name
